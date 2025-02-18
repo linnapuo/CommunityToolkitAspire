3103d8dbb948a0a9bb6aada85219c049778e165e
Teemu Linnapuomi <teemu.linnapuomi@vincit.fi>
2025-02-18 22:59:45 +0200

    Add InfluxDB client library

--- examples/influxdb/CommunityToolkit.Aspire.Hosting.InfluxDB.AppHost/Program.cs
+++ examples/influxdb/CommunityToolkit.Aspire.Hosting.InfluxDB.AppHost/Program.cs
@@ -1,7 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddInfluxDB("influxdb");
+var influx = builder.AddInfluxDB("influxdb");
 
-builder.AddProject<Projects.CommunityToolkit_Aspire_Hosting_InfluxDB_ApiService>("apiservice");
+builder.AddProject<Projects.CommunityToolkit_Aspire_Hosting_InfluxDB_ApiService>("apiservice")
+    .WithReference(influx)
+    .WaitFor(influx);
 
 builder.Build().Run();
